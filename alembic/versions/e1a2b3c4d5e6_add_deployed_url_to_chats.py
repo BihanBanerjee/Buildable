d4b0cd56e16cf3a3558bbf08e01d53af78e9fb87
Bihan Banerjee <banerjeebihan456@gmail.com>
2026-03-24 16:08:54 +0530

    fix: chain deployed_url migration off correct parent to resolve multiple heads

--- alembic/versions/e1a2b3c4d5e6_add_deployed_url_to_chats.py
+++ alembic/versions/e1a2b3c4d5e6_add_deployed_url_to_chats.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = 'e1a2b3c4d5e6'
-down_revision: Union[str, None] = 'd0db556e97f7'
+down_revision: Union[str, None] = 'cf7db7844895'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
